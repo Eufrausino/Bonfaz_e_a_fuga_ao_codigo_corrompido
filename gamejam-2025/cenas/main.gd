da23134f20d5cacc6056af882d13710c23e1b32e
Eufrausino <gustavo.medeiros@ufv.br>
2025-09-25 12:03:26 -0300

    botando as coisas

--- gamejam-2025/cenas/main.gd
+++ gamejam-2025/cenas/main.gd
@@ -27,7 +27,7 @@ var Plataforma = preload("res://cenas/plataforma.tscn")
 var Parede = preload("res://cenas/parede.tscn")
 
 var plataformas : Array
-var plataformasHeights := [0, 10]
+var plataformasHeights := [500, 420]
 var paredes : Array
 
 # Called when the node enters the scene tree for the first time.
@@ -77,8 +77,12 @@ func gerar_obstaculo():
 		var obstaculo = tipoObstaculo.instantiate()
 		
 		var obstaculo_x : int = $Camera2D.position.x + (tam_tela.x / 2) + randi_range(100, 400)
-		var obstaculo_y : int = virusHeights[randi() % virusHeights.size()]
-		
+		var obstaculo_y : int
+		if tipoObstaculo == virus1:
+			obstaculo_y = virusHeights[randi() % virusHeights.size()] - 20
+		else:
+			obstaculo_y = virusHeights[randi() % virusHeights.size()]
+			
 		ultimoObjeto = obstaculo
 		adiciona_obstaculo(obstaculo, obstaculo_x, obstaculo_y)
 		
@@ -103,13 +107,14 @@ func removerObstaculo(obs):
 
 func gerar_platorma():
 	var margem_saida_da_tela = 50 
-	if plataformas.is_empty() or ultimoObjeto.position.x < $Camera2D.position.x + tam_tela.x / 2:
+	if plataformas.is_empty() or ultimoObjeto.position.x < ($Camera2D.position.x + tam_tela.x / 2) - margem_saida_da_tela:
 		
 		#var tipoObstaculo = tipos_obstaculos[randi() % tipos_obstaculos.size()]
 		#var obstaculo = tipoObstaculo.instantiate()
 		var plataforma = Plataforma.instantiate()
-		var x : int = $Camera2D.position.x + (tam_tela.x / 2) + randi_range(10, 400)
+		var x : int = $Camera2D.position.x + (tam_tela.x / 2) + randi_range(100, 500)
 		var y : int = plataformasHeights[randi() % plataformasHeights.size()]
+		print(y)
 		
 		ultimoObjeto = plataforma
 		adiciona_plataforma(plataforma, x, y)
